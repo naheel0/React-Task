da6c8f437000ead2b29b06652119d47fc279c327
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2025-11-30 13:04:09 +0530

    fix: github pages routing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(), 
-  ],
-  base: '/login-page-task/', 
-})
+  plugins: [tailwindcss()],
+  base: '/login-page-task/', // your GitHub Pages repo
 
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('index.html', import.meta.url)),
+        404: fileURLToPath(new URL('index.html', import.meta.url)), // fallback for GitHub Pages
+      },
+    },
+  },
+})
